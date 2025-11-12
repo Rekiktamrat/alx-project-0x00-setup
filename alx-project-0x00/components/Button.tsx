022e40ab15ac0ac44fedc84dcccfce0914dac691
Rekik Tamrat <rekiktamrat72@gmail.com>
2025-11-12 12:04:49 +0300

    try now

--- alx-project-0x00/components/Button.tsx
+++ alx-project-0x00/components/Button.tsx
@@ -1,13 +1,13 @@
-import { ButtonProps } from "@/interfaces";
+import React from 'react';
+import {ButtonProps} from '@/interfaces';
 
-const Button: React.FC<ButtonProps> = ({ title, styles }) => {
-  return (
-    <button
-      className={`px-4 py-2 text-white bg-blue-500 hover:bg-blue-600 transition-colors ${styles}`}
-    >
+const Button: React.FC<ButtonProps> = ({title, styles}) => {
+    return (
+        <button 
+        className={`bg-blue-600 text-white font-semibold hover:bg-blue-700 transition-all duration-200 ${styles}`}>
       {title}
     </button>
-  );
-};
+    );
+}
 
 export default Button;
